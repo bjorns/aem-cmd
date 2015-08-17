75fe8672fcee34aab04e08a49515d0a64a457058
Björn Skoglund <bjorn.skoglund@valtech.se>
2015-08-16 22:39:11 -0400

    Added verbose option to bundle command.

--- acmd/commands/bundle.py
+++ acmd/commands/bundle.py
@@ -2,13 +2,15 @@
 import sys
 import optparse
 import requests
+import json
 
 from acmd.commands.registry import register_command
 from acmd.http_util import get_json, post_form
 
-parser = optparse.OptionParser("acmd [options] <command> <args>")
-parser.add_option("-v", "--verbose", dest="verbose",
-                  help="report verbose data when supported", metavar="bool")
+parser = optparse.OptionParser("acmd bundle [options] [list|start|stop] [<bundle>]")
+parser.add_option("-v", "--verbose",
+                action="store_const", const=True, dest="verbose",
+                help="report verbose data when supported")
 
 
 def get_bundle_list(server):
@@ -16,28 +18,35 @@ def get_bundle_list(server):
     bundles = response['data']
     return bundles
 
-def list_bundles(server):
+def list_bundles(server, options):
     bundles = get_bundle_list(server)
     for bundle in bundles:
-        sys.stdout.write("{bundle}\t{version}\t{status}\n".format(
-            bundle=bundle['symbolicName'],
-            version=bundle['version'],
-            status=bundle['state']))
+        if options.verbose:
+            sys.stdout.write(json.dumps(bundle,indent=4)+"\n")
+        else:
+            sys.stdout.write("{bundle}\t{version}\t{status}\n".format(
+                bundle=bundle['symbolicName'],
+                version=bundle['version'],
+                status=bundle['state']))
 
-def stop_bundle(server, bundlename):
+def stop_bundle(server, bundlename, options):
     """ curl -u admin:admin http://localhost:4505/system/console/bundles/org.apache.sling.scripting.jsp
         -F action=stop."""
     form_data = {'action': 'stop'}
     path = '/system/console/bundles/{bundle}'.format(bundle=bundlename)
-    return post_form(server, path, form_data)
+    resp =  post_form(server, path, form_data)
+    if options.verbose:
+        sys.stdout.write(json.dumps(resp, indent=4) + "\n")
 
 
-def start_bundle(server, bundlename):
+def start_bundle(server, bundlename, options):
     """ curl -u admin:admin http://localhost:4505/system/console/bundles/org.apache.sling.scripting.jsp
         -F action=start."""
     form_data = {'action': 'start'}
     path = '/system/console/bundles/{bundle}'.format(bundle=bundlename)
-    return post_form(server, path, form_data)
+    resp =  post_form(server, path, form_data)
+    if options.verbose:
+        sys.stdout.write(json.dumps(resp, indent=4) + "\n")
 
 
 def get_action(argv):
@@ -63,11 +72,11 @@ class BundleCommand(object):
         action = get_action(args)
         actionarg = get_argument(args)
         if action == 'list':
-            list_bundles(server)
+            list_bundles(server, options)
         elif action == 'start':
-            start_bundle(server, actionarg)
+            start_bundle(server, actionarg, options)
         elif action == 'stop':
-            stop_bundle(server, actionarg)
+            stop_bundle(server, actionarg, options)
         else:
             sys.stderr.write('error: Unknown bundle action {a}\n'.format(a=action))
             sys.exit(-1)
