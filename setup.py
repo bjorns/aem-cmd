e0ebcfe496d3b44f594f343ec8c9bd14dacaeb74
Björn Skoglund <bjorn.skoglund@valtech.se>
2015-08-16 21:57:36 -0400

    Createed setuptools dist.

--- setup.py
+++ setup.py
@@ -1,19 +1,44 @@
+# coding: utf-8
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
+classifiers=[
+    # How mature is this project? Common values are
+    #   3 - Alpha
+    #   4 - Beta
+    #   5 - Production/Stable
+    'Development Status :: 3 - Alpha',
+
+    # Indicate who your project is intended for
+    'Intended Audience :: Developers',
+    'Topic :: Software Development',
+
+    # Pick your license as you wish (should match "license" above)
+     'License :: OSI Approved :: MIT License',
+
+    # Specify the Python versions you support here. In particular, ensure
+    # that you indicate whether you support Python 2, Python 3 or both.
+    'Programming Language :: Python :: 2.7',
+]
+
 config = {
-    'description': 'AEM Command Line Tool',
+    'name': 'aem-cmd',
+    'version': '0.1a3',
+    'description': 'AEM Command line tools',
+    'long_description': 'A set of commands to administer an Adobe AEM content management installation from the command line.',
+    'license': 'MIT',
     'author': 'Björn Skoglund',
-    'url': 'http://twitter.com/bjosk',
-    ## 'download_url': 'Where to download it.',
-    'author_email': 'bjorn.skoglund@valtech.com',
-    'version': '0.1',
-    'install_requires': ['nose', 'requests'],
-    'packages': ['acmd'],
-    'scripts': [],
-    'name': 'aem-cmd'
+    'author_email': 'bjorn.skoglund@icloud.com',
+    'url': 'https://github.com/bjorns/aem-cmd',
+    'download_url': 'https://github.com/bjorns/aem-cmd',
+    'classifiers': classifiers,
+
+    # Build specs
+    'install_requires': ['requests', 'nose'],
+    'packages': ['acmd', 'acmd.commands'],
+    'scripts': ['bin/acmd']
 }
 
 setup(**config)
