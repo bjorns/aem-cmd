4b54a8a4403fafa7f214a054eb86e65576aca128
Björn Skoglund <bjorn.skoglund@valtech.se>
2015-09-24 10:44:08 -0400

    Added bash completion to paths in activate command.

--- acmd/config.py
+++ acmd/config.py
@@ -9,12 +9,15 @@ DEFAULT_SERVER_SETTING = 'default_server'
 
 
 def get_rcfilename():
+    """ Returns the full path of the acmd config file. """
     home = os.path.expanduser("~")
     rcfilename = "{home}/.acmd.rc".format(home=home)
     return rcfilename
 
 
 class Config(object):
+    """ Model of the current config file. """
+
     def __init__(self):
         self.servers = dict()
         self.projects = dict()
@@ -26,23 +29,34 @@ class Config(object):
 
 
 def parse_server(parser, section):
+    """ Parse a [server] entry in the config file. Returns a server.Server object. """
     name = section.split(' ')[1]
 
     host = parser.get(section, 'host')
 
     username = parser.get(section, 'username')
     password = parser.get(section, 'password')
-    dispatcher = parser.get(section, 'dispatcher') if parser.has_option(section, 'dispatcher') else None
+    if parser.has_option(section, 'dispatcher'):
+        dispatcher = parser.get(section, 'dispatcher')
+    else:
+        dispatcher = None
 
-    return acmd.server.Server(name, host=host, username=username, password=password, dispatcher=dispatcher)
+    return acmd.server.Server(name,
+                              host=host,
+                              username=username,
+                              password=password,
+                              dispatcher=dispatcher)
 
 
 def parse_servers(parser):
+    """ Parse all [server] entries in the config file.
+        Returns a dictionary mapping the server names to Server() instances.
+     """
     ret = dict()
     for section in parser.sections():
         if section.startswith('server '):
-            s = parse_server(parser, section)
-            ret[s.name] = s
+            server = parse_server(parser, section)
+            ret[server.name] = server
 
     default_name = parser.get('settings', DEFAULT_SERVER_SETTING)
     ret[DEFAULT_SERVER_SETTING] = ret[default_name]
@@ -50,7 +64,11 @@ def parse_servers(parser):
 
 
 def parse_projects(parser):
-    ret = {}
+    """ Parse all entries under the [projects] section in the config file.
+        Returns a dictionary from the name to the full expanded path of the
+        project dir.
+    """
+    ret = dict()
     if parser.has_section('projects'):
         for name, path in parser.items('projects'):
             path = expanduser(path)
@@ -62,11 +80,13 @@ _current_config = None
 
 
 def get_current_config():
+    """ Return the loaded Config() object. """
     global _current_config
     return _current_config
 
 
 def read_config(filename):
+    """ Read the config file filename. Return a Config() object. """
     global _current_config
     parser = ConfigParser.ConfigParser()
     with open(filename) as f:
