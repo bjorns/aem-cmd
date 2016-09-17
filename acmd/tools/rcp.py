60f3b95e659ba19ba029cdedffc11fe0f69316aa
Björn Skoglund <bjorn.skoglund@valtech.com>
2016-09-17 17:28:15 -0400

    Added create command for rcp

--- acmd/tools/rcp.py
+++ acmd/tools/rcp.py
@@ -34,7 +34,6 @@ class VltRcpTool(object):
 
         Documentation: http://jackrabbit.apache.org/filevault/rcp.html#
     """
-
     def execute(self, server, argv):
         options, args = parser.parse_args(argv)
 
@@ -43,32 +42,49 @@ class VltRcpTool(object):
 
         if action == 'list' or action == 'ls':
             return list_rcp_tasks(server, options)
-        elif action == 'fetch':
-            return fetch_tree_synchronous(server, options, argument)
+        if action == 'create':
+            return create_new_task(server, argument, options)
         elif action == 'start':
             return start_task(server, argument)
         elif action == 'rm':
             return remove_task(server, argument)
         elif action == 'clear':
             clear_tasks(server)
+        elif action == 'fetch':
+            return fetch_tree_synchronous(server, options, argument)
         else:
             error("Unknown rcp action {a}\n".format(a=action))
             return USER_ERROR
 
 
 def list_rcp_tasks(server, options):
-    status, content = get_task_list(server)
+
+    def _get_hostname(url):
+        parts = url.split('/crx')
+        if len(parts) < 2:
+            warning("Failed to split src url {}".format(url))
+            return url
+        host = parts[0]
+        parts = host.split('@')
+        if len(parts) < 2:
+            warning("Failed to retrieve hostname from url {}".format(url))
+            return url
+        return parts[1]
+
+    status, content = _get_task_list(server)
     if status != OK:
         return status
     if options.raw:
         sys.stdout.write("{}\n".format(json.dumps(content, indent=4)))
     else:
         for task in content['tasks']:
-            sys.stdout.write("{}\t{}\t{}\n".format(task['id'], task['dst'], task['status']['state']))
+            sys.stdout.write("{id}\t{src}\t{path}\t{status}\n".format(
+                id=task['id'], src=_get_hostname(task['src']),
+                path=task['dst'], status=task['status']['state']))
     return OK
 
 
-def get_task_list(server):
+def _get_task_list(server):
     url = server.url("/system/jackrabbit/filevault/rcp")
 
     log("GETting service {}".format(url))
@@ -81,36 +97,22 @@ def get_task_list(server):
     return OK, resp.json()
 
 
-def _parse_credentials(creds):
-    """ Takes a credentials string '<user>:<pass>' and returns a tuple (<user>,<pass>) """
-    parts = creds.split(':')
-    if len(parts) != 2:
-        error("Failed to parse credentials: {}".format(creds))
-        sys.exit(-1)
-    return parts[0], parts[1]
+def create_new_task(server, content_path, options):
+    if options.source_host is None:
+        error("Missing argument source host (-s)")
+        return USER_ERROR
 
-
-def fetch_tree_synchronous(server, options, content_path):
     task_id = _create_task_id()
 
-    status = create_task(server, options, task_id, content_path)
+    status, data = _create_task(server, task_id, content_path, options)
     if status != OK:
         error("Failed to fetch {} from {}".format(content_path, options.source_host))
         return status
 
-    status, task = get_task_status(server, task_id)
-    if task['status']['state'] != 'NEW':
-        error("Failed to locsate new task {}".format(task_id))
-        return SERVER_ERROR
-
-    status = start_task(server, task_id)
-    if status != OK:
-        error("Failed to fetch {} from {}, task {} did not start".format(content_path, options.source_host, task_id))
-        return status
-
-    wait_for_task(server, task_id)
-
-    remove_task(server, task_id)
+    if options.raw:
+        sys.stdout.write("{}\n".format(json.dumps(data)))
+    else:
+        sys.stdout.write("{}\n".format(data['id']))
     return OK
 
 
@@ -123,7 +125,7 @@ def _create_task_id():
     return "rcp-{}".format(random_hex(6))
 
 
-def create_task(server, options, task_id, content_path):
+def _create_task(server, task_id, content_path, options):
     """ Create a new task, does not start it """
     log("Creating task {}".format(task_id))
     payload = {
@@ -145,9 +147,9 @@ def create_task(server, options, task_id, content_path):
     resp = requests.post(url, auth=server.auth, json=payload)
     if resp.status_code != 201:
         error("Failed to create task, request returned {} and {}\n".format(resp.status_code, resp.content))
-        return SERVER_ERROR
+        return SERVER_ERROR, None
 
-    return OK
+    return OK, resp.json()
 
 
 def start_task(server, task_id):
@@ -166,7 +168,7 @@ def remove_task(server, task_id):
 
 def clear_tasks(server):
     """ Remove all tasks """
-    status, content = get_task_list(server)
+    status, content = _get_task_list(server)
     if status != OK:
         return status
     for task in content['tasks']:
@@ -179,7 +181,7 @@ def clear_tasks(server):
 
 def get_task_status(server, task_id):
     """ Return all data on a single task """
-    status, content = get_task_list(server)
+    status, content = _get_task_list(server)
     for task in content['tasks']:
         if task['id'] == task_id:
             return OK, task
@@ -219,3 +221,32 @@ def post_command(server, task_id, cmd):
         return SERVER_ERROR, resp.json()
 
     return OK, resp.json()
+
+
+def fetch_tree_synchronous(server, options, content_path):
+    """ Compount tool for fetching data immediately.
+        Performs: Create, Fetch, Wait, Remove
+    """
+    task_id = _create_task_id()
+
+    status, data = _create_task(server, task_id, content_path, options)
+    if status != OK:
+        error("Faiiled to fetch {} from {}".format(content_path, options.source_host))
+        return status
+
+    status, task = get_task_status(server, task_id)
+    if task['status']['state'] != 'NEW':
+        error("Failed to locsate new task {}".format(task_id))
+        return SERVER_ERROR
+
+    status = start_task(server, task_id)
+    if status != OK:
+        error("Failed to fetch {} from {}, task {} did not start".format(content_path, options.source_host, task_id))
+        return status
+
+    wait_for_task(server, task_id)
+
+    remove_task(server, task_id)
+    return OK
+
+
--- tests/tools/test_rcp.py
+++ tests/tools/test_rcp.py
@@ -1,10 +1,10 @@
 # coding: utf-8
 import json
 from StringIO import StringIO
-from httmock import urlmatch, HTTMock
 
-from nose.tools import eq_
+from httmock import urlmatch, HTTMock
 from mock import patch
+from nose.tools import eq_
 
 from acmd import get_tool, Server
 
@@ -13,6 +13,7 @@ LS_RESPONSE = {
         {
             'id': 'task_id_4711',
             'dst': '/content/dam/something',
+            "src": "http://admin:admin@localhost:4503/crx/server/-/jcr:root/content/dam/something",
             'status': {
                 'state': 'RUNNING'
             }
@@ -20,6 +21,7 @@ LS_RESPONSE = {
         {
             'id': 'task_id_4712',
             'dst': '/content/dam/something_else',
+            "src": "http://admin:admin@localhost:4503/crx/server/-/jcr:root/content/dam/something",
             'status': {
                 'state': 'CREATED'
             }
@@ -30,20 +32,63 @@ LS_RESPONSE = {
 
 
 @urlmatch(netloc='localhost:4502', method='GET')
-def service_mock(url, request):
+def get_service_mock(url, request):
     if url.path == '/system/jackrabbit/filevault/rcp':
         return json.dumps(LS_RESPONSE)
     else:
         raise Exception(url.path)
 
 
+posted_data = None
+
+
+
+def tabbed(lines):
+    lines = ['\t'.join(l) for l in lines]
+    return '\n'.join(lines) + '\n'
+
+
 @patch('sys.stdout', new_callable=StringIO)
 @patch('sys.stderr', new_callable=StringIO)
 def test_rcp_ls(stderr, stdout):
-    with HTTMock(service_mock):
+    with HTTMock(get_service_mock):
         tool = get_tool('rcp')
         server = Server('localhost')
         status = tool.execute(server, ['ls'])
         eq_(0, status)
-        eq_('task_id_4711\t/content/dam/something\tRUNNING\ntask_id_4712\t/content/dam/something_else\tCREATED\n', stdout.getvalue())
+        lines = [
+            ['task_id_4711', 'localhost:4503', '/content/dam/something', 'RUNNING'],
+            ['task_id_4712', 'localhost:4503', '/content/dam/something_else', 'CREATED']
+        ]
+        eq_(tabbed(lines), stdout.getvalue())
+        eq_('', stderr.getvalue())
+
+
+@urlmatch(netloc='localhost:4502', method='POST')
+def post_service_mock(url, request):
+    global posted_data
+
+    if url.path == '/system/jackrabbit/filevault/rcp':
+        posted_data = json.loads(request.body)
+        return {
+            'status_code': 201,
+            'content': '{"status": "ok", "id": "rcp-0ed9f8"}'
+        }
+
+    else:
+        raise Exception("Unexpected post uri {}".format(url))
+
+
+@patch('sys.stdout', new_callable=StringIO)
+@patch('sys.stderr', new_callable=StringIO)
+def test_rcp_create(stderr, stdout):
+    with HTTMock(post_service_mock):
+        tool = get_tool('rcp')
+        server = Server('localhost')
+        status = tool.execute(server, ['rcp', 'create', '-s', 'other-host:4502', '-c', 'user:pass', '/content/dam/data'])
+
+        eq_('http://user:pass@other-host:4502/crx/-/jcr:root/content/dam/data', posted_data['src'])
+
+        eq_(0, status)
+        eq_('rcp-0ed9f8\n', stdout.getvalue())
         eq_('', stderr.getvalue())
