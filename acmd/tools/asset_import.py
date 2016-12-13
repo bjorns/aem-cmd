4d584e22127fcfe327f5756041b41f66aace7204
Björn Skoglund <bjorn.skoglund@valtech.com>
2016-12-13 14:57:55 -0500

    Simplifying assets tool

--- acmd/tools/asset_import.py
+++ acmd/tools/asset_import.py
@@ -1,6 +1,4 @@
 # coding: utf-8
-import datetime
-import hashlib
 import mimetypes
 import os
 import re
@@ -17,49 +15,6 @@ ROOT_IMPORT_DIR = "/tmp/acmd_assets_ingest"
 
 class AssetException(Exception):
     pass
-
-
-class UploadRegistry(object):
-    def __init__(self, server, path, force_root=None):
-        if force_root is not None:
-            self.lock_dir = force_root
-        else:
-            self.lock_dir = os.path.join(ROOT_IMPORT_DIR, self._hash_job(server, path))
-        log("Cache dir is {}".format(self.lock_dir))
-
-    @staticmethod
-    def _hash_job(server, path):
-        """ Produce unique folder for upload based on path and server """
-        return hashlib.sha1('{}:{}'.format(server.name, path)).hexdigest()[:8]
-
-    def is_uploaded(self, filepath):
-        lock_file = self._lock_file(filepath)
-        log("Checking lock file {}".format(lock_file))
-        return os.path.exists(lock_file)
-
-    def _lock_file(self, filepath, os_name=os.name):
-        """ Return the filepath to the lock file for a given file """
-        if filepath.startswith('/'):
-            filepath = filepath[1:]
-        return os.path.join(self.lock_dir, filepath)
-
-    def mark_uploaded(self, filepath, dry_run=False):
-        lock_file = self._lock_file(filepath, dry_run)
-        self._touch(lock_file, dry_run)
-
-    @staticmethod
-    def _touch(filename, dry_run=False):
-        """ Create empty file """
-        assert filename.startswith("/tmp")
-        par_dir = os.path.dirname(filename)
-        if dry_run:
-            log("Skipping creating dir {} because dry run".format(par_dir))
-            return
-        if not os.path.exists(par_dir):
-            log("Creating directory {}".format(par_dir))
-            os.makedirs(par_dir, mode=0755)
-        log("Creating lock file {}".format(filename))
-        open(filename, 'a').close()
 
 
 def get_dam_path(filepath, local_import_root, dam_import_root):
@@ -84,15 +39,6 @@ def clean_path(path):
     if pattern.match(ret) is None:
         raise AssetException("File path {} contains unallowed characters".format(path))
     return ret
-
-
-def format_timestamp(t):
-    return datetime.datetime.fromtimestamp(t).strftime('%Y-%m-%d %H:%M:%S')
-
-
-def hash_job(server, path):
-    """ Produce unique folder for upload based on path and server """
-    return hashlib.sha1('{}:{}'.format(server.name, path)).hexdigest()[:8]
 
 
 def create_dir(server, path, dry_run):
@@ -145,14 +91,3 @@ def filter_unwanted(filename):
 def _ok(status_code):
     """ Returns true if http status code is considered success """
     return status_code == 200 or status_code == 201
-
-
-def count_files(dirpath):
-    """ Return the number of files in directory """
-    log("Counting files...")
-    i = 0
-    for subdir, dirs, files in os.walk(dirpath):
-        for filename in files:
-            if not filter_unwanted(filename):
-                i += 1
-    return i
--- acmd/tools/assets.py
+++ acmd/tools/assets.py
@@ -1,14 +1,11 @@
 # coding: utf-8
-import optparse
-
 import sys
-import time
+import optparse
 
 from acmd import USER_ERROR, SERVER_ERROR
 from acmd import tool, error
 from acmd.tools.tool_utils import get_argument, get_command
 from acmd.tools.asset_import import *
-from acmd.filter import FileFilter
 
 parser = optparse.OptionParser("acmd assets <import|touch> [options] <file>")
 parser.add_option("-r", "--raw",
@@ -19,12 +16,6 @@ parser.add_option("-D", "--dry-run",
                   help="Do not change repository")
 parser.add_option("-d", "--destination", dest="destination_root",
                   help="The root directory to import to")
-parser.add_option("-q", "--local-root", dest="local_root",
-                  help="The local root directory to map to the destination root")
-parser.add_option("-l", "--lock-dir", dest="lock_dir",
-                  help="Directory to store information on uploaded files")
-parser.add_option("-f", "--filter", dest="filter_file",
-                  help="Read filtering config from json file")
 
 
 @tool('assets')
@@ -33,10 +24,7 @@ class AssetsTool(object):
 
     def __init__(self):
         self.created_paths = set([])
-        self.total_files = 1
         self.current_file = 1
-        self.upload_registry = None
-        self.file_filter = None
 
     def execute(self, server, argv):
         options, args = parser.parse_args(argv)
@@ -52,9 +40,6 @@ class AssetsTool(object):
 
     def import_path(self, server, options, path):
         """ Import generic file system path, could be file or dir """
-        self.upload_registry = UploadRegistry(server, path, options.lock_dir)
-        if options.filter_file:
-            self.file_filter = FileFilter(options.filter_file)
         if options.dry_run:
             log("Dry running import")
 
@@ -73,11 +58,6 @@ class AssetsTool(object):
         if import_dir.endswith("/"):
             import_dir = import_dir[:-1]
 
-        local_root = options.local_root if options.local_root else import_dir
-        log("Local root set to {}".format(local_root))
-        self.total_files = count_files(import_dir)
-        log("Importing {n} files in {path}".format(n=self.total_files, path=import_dir))
-
         status = OK
         for subdir, dirs, files in os.walk(import_dir):
             # _create_dir(server, subdir)
@@ -87,7 +67,7 @@ class AssetsTool(object):
                     if filter_unwanted(filename):
                         log("Skipping {path}".format(path=filepath))
                         continue
-                    self.import_file(server, options, local_root, filepath)
+                    self.import_file(server, options, import_dir, filepath)
                 except AssetException as e:
                     error("Failed to import {}: {}".format(filepath, e.message))
                     status = SERVER_ERROR
@@ -97,24 +77,6 @@ class AssetsTool(object):
     def import_file(self, server, options, local_import_root, filepath):
         """ Import single file """
         assert os.path.isfile(filepath)
-        t0 = time.time()
-
-        if self.file_filter and not self.file_filter.accept(filepath):
-            msg = "{ts}\t{i}/{n}\tFiltered {local}".format(ts=format_timestamp(time.time()),
-                                                             i=self.current_file,
-                                                             n=self.total_files,
-                                                             local=filepath)
-            log(msg)
-            return OK
-
-        if self.upload_registry.is_uploaded(filepath):
-            msg = "{ts}\t{i}/{n}\tSkipping {local}\n".format(ts=format_timestamp(time.time()),
-                                                             i=self.current_file,
-                                                             n=self.total_files,
-                                                             local=filepath)
-            sys.stdout.write(msg)
-            sys.stdout.flush()
-            return OK
 
         dam_path = get_dam_path(filepath, local_import_root, options.destination_root)
 
@@ -127,13 +89,6 @@ class AssetsTool(object):
             log("Skipping creating dam path {}".format(dam_path))
 
         post_file(server, filepath, dam_path, options.dry_run)
-        t1 = time.time()
-        benchmark = '{0:.3g}'.format(t1 - t0)
-        sys.stdout.write("{ts}\t{i}/{n}\t{local} -> {dam}\t{benchmark}\n".format(ts=format_timestamp(t1),
-                                                                                 i=self.current_file,
-                                                                                 n=self.total_files,
-                                                                                 local=filepath, dam=dam_path,
-                                                                                 benchmark=benchmark))
+        sys.stdout.write("{local} -> {dam}\n".format(local=filepath, dam=dam_path))
         sys.stdout.flush()
-        self.upload_registry.mark_uploaded(filepath, options.dry_run)
         return OK
