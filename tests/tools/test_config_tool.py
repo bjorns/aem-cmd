b31d502167881628ac213639e582cfbd6dd3e9e1
Björn Skoglund <bjorn.skoglund@icloud.com>
2017-10-04 18:41:52 -0400

    Added encrypt decrypt test for config tool

--- tests/tools/test_config_tool.py
+++ tests/tools/test_config_tool.py
@@ -9,13 +9,15 @@ from acmd import OK
 from acmd import tool_repo, Server
 from acmd.tools.config_tool import encrypt_str, decrypt_str
 
-CONFIG = """[settings]
+PLAINTEXT_CONFIG = """[settings]
 default_server = local
 
 [server localhost]
 host = http://localhost:4502
 username = admin
-password = admin"""
+password = admin
+
+"""
 
 ENCRYPTED_CONFIG = """[settings]
 default_server = local
@@ -28,34 +30,50 @@ password_iv = MDEyMzQ1Njc4OWFiY2RlZg==
 
 """
 
+
 @patch('getpass.getpass')
 @patch('acmd.tools.config_tool.get_iv')
 def test_encrypt_password(get_iv, getpass):
     getpass.return_value = "foobarpass"
     get_iv.return_value = b'0123456789abcdef'
-
     _, tmp_filepath = tempfile.mkstemp(".rc")
     with open(tmp_filepath, 'w') as f:
-        f.write(CONFIG)
-
+        f.write(PLAINTEXT_CONFIG)
     tool = tool_repo.get_tool('config')
     server = Server('localhost')
     ret = tool.execute(server, ['config', 'encrypt', tmp_filepath])
     eq_(OK, ret)
-
     with open(tmp_filepath, 'r') as f:
         data = f.read()
-
     eq_(ENCRYPTED_CONFIG, data)
+    os.remove(tmp_filepath)
 
+
+@patch('getpass.getpass')
+@patch('acmd.tools.config_tool.get_iv')
+def test_encrypt_decrypt(get_iv, getpass):
+    getpass.return_value = "somepassword"
+    get_iv.return_value = b'fedcba0987654321'
+    _, tmp_filepath = tempfile.mkstemp(".rc")
+    with open(tmp_filepath, 'w') as f:
+        f.write(PLAINTEXT_CONFIG)
+    tool = tool_repo.get_tool('config')
+    server = Server('localhost')
+    ret = tool.execute(server, ['config', 'encrypt', tmp_filepath])
+    eq_(OK, ret)
+    ret = tool.execute(server, ['config', 'decrypt', tmp_filepath])
+    eq_(OK, ret)
+    with open(tmp_filepath, 'r') as f:
+        data = f.read()
+    eq_(PLAINTEXT_CONFIG, data)
     os.remove(tmp_filepath)
 
 
 def test_encrypt_decrypt_str():
-    IV = b'This is an IV456'
+    iv = b'This is an IV456'
     msg = "Hello Wörld"
     key = b'Some kind of key'
-    data = encrypt_str(IV, key, msg)
+    data = encrypt_str(iv, key, msg)
     eq_("SapMLwk/NqcHuy4y", data)
-    new_msg = decrypt_str(IV, key, data)
+    new_msg = decrypt_str(iv, key, data)
     eq_(msg, new_msg)
